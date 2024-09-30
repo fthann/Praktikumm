1c0b7447c9be3e1a1f0e47e19e1dca4a1c0b78fa
fthann <fathanalfrq@gmail.com>
2024-09-30 14:23:18 +0700

    changes bjrott

--- Program.cs
+++ Program.cs
@@ -1,2 +1,46 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+// Variable dan tipe data (int)
+
+int angka1 = 3;
+int angka3;
+angka3 = 9;
+int angka2 = 8;
+Console.WriteLine(angka1);
+Console.WriteLine(angka2);
+Console.WriteLine(angka3);
+
+// Variable dan tipe data (string)
+
+string nama;
+nama = "Jablay";
+string nim = "123456789";
+Console.WriteLine(nama);
+Console.WriteLine(nim);
+
+
+// Variable dan tipe data (char)
+
+char alphabet1 = 'A';
+char alphabet2 = 'B';
+
+// Variable dan tipe data (float)
+
+float decimal1 = 5.6f;
+float decimal2 = 6.6f;
+
+// Variable dan tipe data (double)
+
+double double_decimal1 = 4.3;
+double double_decimal2 = 3.4;
+
+// Variable dan tipe data (boolean) hanya berisi true and false
+
+bool trueResult = true;
+bool falseResult = false;
+
+// Variable dan tipe data (decimal)
+
+decimal decimalResult1 = 3.4m;
+decimal decimalResult2 = 5.4m;
